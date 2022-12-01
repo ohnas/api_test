370d9a9b814b99adf8a62a593689a24432cd2dc6
ohnas <ohnas12@gmail.com>
2022-12-01 15:12:14 +0900

    test1

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-const newDiv = document.createElement("div");
-
-newDiv.innerHTML = "내가 만든 div";
-document.body.appendChild(newDiv);
+//1. 자기 호출 함수 사용
+(function(){
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "내가 만든 divdivdiv";
+    document.body.appendChild(newDiv);
+ }());
+ 
+ //2. addEventListener 사용
+ window.addEventListener("load",function(event) {
+ },false);
+ 
